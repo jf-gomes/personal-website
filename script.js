c6c72094347f56e34c6be85cefd8359cc4c6409c
João Víctor Gomes <131728008+jf-gomes@users.noreply.github.com>
2023-10-26 18:19:41 -0400

    added repositories

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ const loop = setInterval(() => {
         skillsContent.style.flexWrap = 'nowrap';
     }
 }, 10);
+
+let reposContent = document.getElementById('repos-content');
+
+fetch('https://api.github.com/users/jf-gomes/repos')
+.then(function (serverResponse){
+    return serverResponse.json();
+})
+.then(function (convertedResponse){
+    console.log(convertedResponse);
+    totalRepos = convertedResponse.length;
+    for (c = 0; c < totalRepos; c ++){
+        reposContent.innerHTML += `<a href="${convertedResponse[c].svn_url}" target="_blank">${convertedResponse[c].name}</a>`;
+    }
+})
